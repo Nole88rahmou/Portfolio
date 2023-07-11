4d9a2fd7fcdf4dfeb245846e8e8d295a4f884ab6
as <as@DESKTOP-9MCK3ND>
2023-07-11 02:45:22 -0400

    creation du footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+function Footer  ()  {
+    return (
+        <footer className="footer">
+            <a href="https://github.com/Nole88rahmou">GitHub</a>
+            <a href="https://www.linkedin.com/in/meradji-abderrahim-3bb444251">LinkedIn</a>
+        </footer>
+    );
+};
+
+export default Footer;
